3c77c81129a2b2e5827bf7cadbf2a74d7a07c8da
Larissa Kich <76120366+Larissakich@users.noreply.github.com>
2022-11-02 19:13:15 -0300

    Add files via upload

---
+++ dark light form/assets/js/script.js
@@ -0,0 +1,18 @@
+const mode = document.getElementById('mode_icon');
+
+mode.addEventListener('click', () => {
+    const form = document.getElementById('login_form');
+
+    if(mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+
+        form.classList.add('dark');
+        return ;
+    }
+    
+    mode.classList.remove('fa-sun');
+    mode.classList.add('fa-moon');
+
+    form.classList.remove('dark');
+});
